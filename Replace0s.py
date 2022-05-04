edccaa5c0bd19bf4d0a46ac65112394b04cf8152
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-05-04 15:06:21 -0400

    Update Replace0s.py

--- Replace0s.py
+++ Replace0s.py
@@ -1,28 +1,19 @@
 #Replace all 0's with 5 for a given integer
 #Practice problem for geeksforgeeks.org
 
-def convertFive(n):
-    cont=0
-    a=str(n)
-    c=len(a)
-    nf=0
-    for i in a:
-        cont+=1
-        lugar=c-cont
-        b=int(i)
-        if b==0:
-            b=5
+
+def convert(n):
+    new = []
+    for i in str(n):
+        if i == '0':
+            new.append('5')
         else:
-            pass
-        nf=nf+b*10**(lugar)
-    #print("initial number is: ",n)
-    #print("final number is: ",nf)
-    n=nf
-    return n
+            new.append(i)
+    #print(new)
+    new = int(''.join(new))
+    return new
 
 
 if __name__=='__main__':
-    t=int(input("t: "))
-    for i in range (t):
-        n=int(input("n: "))
-        print(convertFive(n))
+    n = int(input("What in the number? "))
+    print(convert(n))
