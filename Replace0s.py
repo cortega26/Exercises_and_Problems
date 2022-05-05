6e540de539dd1a586c1b550aebb98efcba66820d
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-05-05 15:07:43 -0400

    Update Replace0s.py

--- Replace0s.py
+++ Replace0s.py
@@ -2,6 +2,8 @@
 #Practice problem for geeksforgeeks.org
 
 
+#Convert all 0's to 5's in a given integer
+
 def convert(n):
     new = []
     for i in str(n):
@@ -9,11 +11,24 @@ def convert(n):
             new.append('5')
         else:
             new.append(i)
-    #print(new)
-    new = int(''.join(new))
-    return new
+    return int(''.join(new))
 
 
 if __name__=='__main__':
-    n = int(input("What in the number? "))
-    print(convert(n))
+    print("Do you want to include leading zeros (if exist)?")
+    answer = input()
+    if answer.upper() == "YES" or answer.upper() == "Y":
+        n = str(input("What in the number? "))
+        try:
+            a = int(n)
+            print(convert(n))
+        except:
+            print("Not a number. Program terminated.")
+    elif answer.upper() == "NO" or answer.upper() == "N":
+        try:
+            n = int(input("What in the number? "))
+            print(convert(n))
+        except:
+            print("Not a number. Program terminated.")
+    else:
+        print("Invalid answer. Program terminated.")
