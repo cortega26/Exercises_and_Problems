538d6390b597e17da8403fea34b7af92ec7c2134
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-06-13 16:11:02 -0400

    Create LeadersInArray.py

---
+++ LeadersInArray.py
@@ -0,0 +1,15 @@
+# Leaders in array
+#
+# Given an array A of positive integers. Your task is to find the leaders in the array.
+# An element of array is leader if it is greater than or equal to all the elements to
+# its right side. The rightmost element is always a leader. 
+
+def leaders(A, N):
+    sol = []
+    max = 0
+    for i in reversed(A):
+        if i > max:
+            sol.append(i)
+            max = i    
+    sol = sol[::-1]
+    return sol
