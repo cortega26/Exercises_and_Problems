62b7dcd32404cc4012a9058e90e2d0ee1c81e172
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-06-15 14:27:41 -0400

    Create countPairs.py

---
+++ countPairs.py
@@ -0,0 +1,18 @@
+# Given an array of N integers, and an integer K, find the number of
+# pairs of elements in the array whose sum is equal to K.
+# Expected Time Complexity: O(n)
+
+def getPairsCount(self, arr, n, k):
+    counter = 0
+    dict_list = {}
+    for i in range(n):
+        if arr[i] in dict_list.keys():
+            dict_list[arr[i]] += 1
+        else:
+            dict_list[arr[i]] = 1
+        if (k - arr[i]) in dict_list.keys():
+            if k - arr[i] != arr[i]:
+                counter +=  dict_list[k - arr[i]]
+            elif dict_list[arr[i]] > 1:
+                counter += dict_list[arr[i]] - 1
+    return counter
