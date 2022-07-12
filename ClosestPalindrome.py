964f78cab0aea019e0eea699fdbd5c9f15dcf341
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-07-12 12:12:40 -0400

    Update to PEP-8

--- ClosestPalindrome.py
+++ ClosestPalindrome.py
@@ -1,119 +1,45 @@
+def nearestPalindromic(n: str):
+    """
+    Given a string n representing an integer, return the closest integer 
+    (not including itself), which is a palindrome. If there is a tie, 
+    return the smaller one.
+    """
 
-import time
-import math
-start_time = time.time()
-
-def Palindrome(Strnum):
-    for i in range(len(Strnum) // 2):
-        if Strnum[i] != Strnum[-1 - i]:
-            return False
-    return True
-
-
-def CutInHalf(Strnum):
-    Half1b=0
-    cont=0       
-    for i in range(math.ceil(len(Strnum) / 2)):            
-        cont+=1
-        Half1b = (10 * Half1b) + int(Strnum[i])
-    Half2b=Half1b
-    test_num = test_numU = test_numL = 0
-    if len(str(Strnum)) % 2 == 0:
-        Upper=Half1b+1
-        Lower=Half1b-1
-        while Half2b > 0:
-            remainder = Half2b % 10
-            test_num = (test_num * 10) + remainder
-            Half2b = Half2b//10
-        while Upper > 0:
-            remainder = Upper % 10
-            test_numU = (test_numU * 10) + remainder
-            Upper = Upper//10
-        while Lower > 0:
-            remainder = Lower % 10
-            test_numL = (test_numL * 10) + remainder
-            Lower = Lower//10
-        A=(Half1b*(10**(cont)))+test_num
-        B=((Half1b+1)*(10**(cont)))+test_numU
-        C=((Half1b-1)*(10**(cont)))+test_numL
-    else:        
-        Upper=Half2b+1
-        Lower=Half2b-1
-        while Half2b > 0:
-            remainder = Half2b % 10
-            test_num = (test_num * 10) + remainder
-            Half2b = Half2b//10
-        while Upper > 0:
-            remainder = Upper % 10
-            test_numU = (test_numU * 10) + remainder
-            Upper = Upper//10
-        while Lower > 0:
-            remainder = Lower % 10
-            test_numL = (test_numL * 10) + remainder
-            Lower = Lower//10
-        test_numnew = test_numUnew = test_numLnew = 0
-        if len(str(Half1b))==len(str(test_numU)):
-            test_numU=str(test_numU)
-            for i in range(len(test_numU)):
-                test_numUnew=int(test_numU[1:i]+test_numU[i:])
-        else:
-            test_numUnew=test_numU
-        if len(str(Half1b))==len(str(test_num)):
-            test_num=str(test_num)
-            for i in range(len(test_num)):
-                test_numnew=int(test_num[1:i]+test_num[i:])
-        else:
-            test_numnew=test_num
-        if len(str(Half1b))==len(str(test_numL)):
-            test_numL=str(test_numL)
-            for i in range(len(test_numL)):
-                test_numLnew=int(test_numL[1:i]+test_numL[i:])
-        else:
-            test_numLnew=test_numL             
-        B=(Half1b*(10**(cont-1)))+test_numnew
-        C=((Half1b+1)*(10**(cont-1)))+test_numUnew
-        A=((Half1b-1)*(10**(cont-1)))+test_numLnew        
-    #print(A,B,C)
-    #print(abs(A-N))
-    #print(abs(B-N))
-    #print(abs(C-N))
-    if abs(A-N) <= abs(B-N) and (A-N) <= abs(C-N):
-        return A
-    elif abs(B-N) <= abs(A-N) and abs(B-N) <= abs(C-N):
-        return B
-    else:
-        return C
-    
-                
-def convertNumIntoString(N):
-	Strnum = str(N)
-	return Strnum        
-
-
-def ClosestPalindrome(N):
-    change=-1
-    for i in range (N):    
-        if Palindrome(convertNumIntoString(N)) == True:
-            return N
-        else:
-            N=N+change
-            if change > 0:
-                change+=1
-                change=-change
-            else:
-                change-=1
-                change=-change
-
-#T=int(input())
-T=1
-for i in range(T):
-    #N=int(input())
-    N=941648184679874984126489754681841478988811111
-    if N == 0:
-        print("0")        
-    elif N < 100:
-        print(ClosestPalindrome(N))
+    size = len(n)
+    n = int(n)
+    if size == 1:
+        return str(n - 1)
+    if size == 2 and str(n) == str(n)[::-1]:
+        for i in range(1,6):
+            if str(n - i) == str(n - i)[::-1]:
+                return str(n - i)
+            elif str(n + i) == str(n + i)[::-1]:
+                return str(n + i)
         
+    half = (size // 2) + size % 2 
+    half_up = int(str(n)[:half]) + 1
+    half_mid = int(str(n)[:half])
+    half_down = int(str(n)[:half]) - 1
+    if size % 2 == 0:
+        up = int(str(half_up) + str(half_up)[::-1])
+        mid = int(str(half_mid) + str(half_mid)[::-1])
+        down = int(str(half_down) + str(half_down)[::-1])
     else:
-        print((CutInHalf(convertNumIntoString(N))))
-print("--- %s seconds ---" % (time.time() - start_time))        
+        if len(str(half_up)) != half:
+            up = int(str(half_up)[:-2] + str(half_up)[::-1])
+        else:
+            up = int(str(half_up)[:-1] + str(half_up)[::-1])
+        mid = int(str(half_mid)[:-1] + str(half_mid)[::-1])
+        if len(str(half_down)) != half:
+            down = int(str(half_down) + str(half_down)[::-1])
+        else:
+            down = int(str(half_down)[:-1] + str(half_down)[::-1])
+
+    min_diff = min(abs(n - up), abs(n - mid), abs(n - down))
+    if min_diff == 0:
+        min_diff = min(abs(n - up), abs(n - down))     
+        
+    if str(n - min_diff) == str(n - min_diff)[::-1]:
+        return str(n - min_diff)
+    else:
+        return str(n + min_diff)
