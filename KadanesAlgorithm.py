2ef426be9bd61dfd9f5a9fc6f1ca92908bbbafe9
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-07-12 12:55:22 -0400

    Update KadanesAlgorithm.py

--- KadanesAlgorithm.py
+++ KadanesAlgorithm.py
@@ -1,12 +1,14 @@
-# Given an array arr[] of N integers. Find the contiguous sub-array
-# (containing at least one number) which has the maximum sum and
-# return its sum.
-
 def maxSubArraySum(arr, N):
+    """
+    Given an array arr[] of N integers. Find the contiguous
+    sub-array (containing at least one number) which has the 
+    maximum sum and return its sum.
+    """
+
+    if max(arr) <= 0: return max(arr)
+    
     max_sum = 0
     current_sum = 0
-    if max(arr) <= 0:
-        return max(arr)
     for i in arr:
         current_sum += i
         if current_sum > max_sum:
