d76c57f074a93d59c953065572f875f8b8a86670
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-07-14 17:20:54 -0400

    Update first_repeating_element.py

--- first_repeating_element.py
+++ first_repeating_element.py
@@ -1,18 +1,21 @@
-# Given an array arr[] of size n, find the first repeating element.
-# The element should occurs more than once and the index of its
-# first occurrence should be the smallest.
+# https://practice.geeksforgeeks.org/problems/first-repeating-element4018/1
 
 def firstRepeated(arr, n):
+    """
+    Given an array arr[] of size n, find the first repeating element.
+    The element should occurs more than once and the index of its
+    first occurrence should be the smallest.
+    """
+    
     mp = {}
     for i in range(n):
         if arr[i] in mp.keys():
             mp[arr[i]] += 1
-            break
         else:
             mp[arr[i]] = 1
     for i in range(n):
         if mp[arr[i]] >= 2:
-            return i
+            return i + 1
     return -1
 
 if __name__ == "__main__":
