2b5bdb8f9b7ddcdce720268c1c839be8510fa4d7
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-07-18 11:38:40 -0400

    PEP-8 update

--- reverseArrayInGroups.py
+++ reverseArrayInGroups.py
@@ -1,17 +1,18 @@
-# Level: Basic
-# Given an array arr[] of positive integers of size N. Reverse every
-# sub-array group of size K.
-
+# https://practice.geeksforgeeks.org/problems/reverse-array-in-groups0255/1
 
 def reverseInGroups(arr, N, K):
+    """
+    Given an array arr[] of positive integers of size N. Reverse every 
+    sub-array group of size K.
+    """
   
-    # Number of sub-arrays to reverse
-    if N % K == 0:
-        subs = N // K
-    else:
-        subs = (N // K) + 1
-        
+    subs = N // K
+    if N % K != 0:
+        subs += 1
     solution = []
     for i in range(subs):
         solution += arr[K * i : (K * i) + K][::-1]    
     return solution
+  
+if __name__ == "__main__":
+  reverseInGroups([1,2,3,4,5,6,7,8,9,10],10,3)
