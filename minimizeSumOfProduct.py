d3b947d19b82203ba257ddedb791bd923662abbf
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-07-18 17:20:53 -0400

    PEP-8 update

--- minimizeSumOfProduct.py
+++ minimizeSumOfProduct.py
@@ -1,11 +1,14 @@
-# You are given two arrays, A and B, of equal size N. The task is to find the minimum
-# value of A[0] * B[0] + A[1] * B[1] +…+ A[N-1] * B[N-1], where shuffling of elements
-# of arrays A and B is allowed.
+# https://practice.geeksforgeeks.org/problems/minimize-the-sum-of-product1525/1
 
 def minValue(a, b, n):
+    """
+    You are given two arrays, A and B, of equal size N. The task is to find 
+    the minimum value of A[0] * B[0] + A[1] * B[1] + ... + A[N-1] * B[N-1], 
+    where shuffling of elements of arrays A and B is allowed.
+    """
     a.sort()
     b.sort(reverse = True)
-    sum_of_product = 0
-    for i in range(n):
-        sum_of_product += a[i] * b[i]
-    return sum_of_product
+    return sum([a[i] * b[i] for i in range(n)])
+
+if __name__ == "__main__":
+    minValue([1,6,2],[14,19,8],3)
