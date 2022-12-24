ca2ee1acd3201fd7e7fc7cb6fb6056c62a16fd08
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-12-23 21:23:16 -0300

    Update reverse_array_in_groups.py

--- reverse_array_in_groups.py
+++ reverse_array_in_groups.py
@@ -1,17 +1,20 @@
 # https://practice.geeksforgeeks.org/problems/reverse-array-in-groups0255/1
 
-def reverseInGroups(arr, N, K):
+def reverse_in_groups(arr: List[int], N: int, K: int) -> List[int]:
     """
-    Given an array arr[] of positive integers of size N. Reverse every 
-    sub-array group of size K.
+    Given a list of positive integers arr of size N, reverse every 
+    sub-list of size K.
     """
-    subs = N // K
+    # Calculate the number of sub-lists
+    num_sublists = N // K
     if N % K != 0:
-        subs += 1
-    solution = []
-    for i in range(subs):
-        solution += arr[K * i : (K * i) + K][::-1]    
-    return solution
+        num_sublists += 1
+    
+    # Reverse each sub-list and append it to the result list
+    result = []
+    for i in range(num_sublists):
+        result += arr[K * i : (K * i) + K][::-1]    
+    return result
   
 if __name__ == "__main__":
-    reverseInGroups([1,2,3,4,5,6,7,8,9,10],10,3)
+    reverse_in_groups([1,2,3,4,5,6,7,8,9,10], 10, 3)
