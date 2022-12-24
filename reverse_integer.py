e4cac3a8ddd26a6b2471c0185ce8d879efc952bf
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-12-23 21:20:00 -0300

    Update reverse_integer.py

--- reverse_integer.py
+++ reverse_integer.py
@@ -1,21 +1,21 @@
 # https://leetcode.com/problems/reverse-integer/
 
-def reverse(x):
+def reverse(x: int) -> int:
     """
     Given a signed 32-bit integer x, return x with its digits reversed.
     If reversing x causes the value to go outside the signed 32-bit 
     integer range [-2E31, 2E31 - 1], then return 0.
     """
-    
     if x >= 0:
-        x = int(str(x)[::-1])
+        reversed_x = int(str(x)[::-1])
     else:
-        x = -1 * int(str(x)[1:][::-1])        
-    if -1 * (2**31) <= x <= (2**31) - 1:
-        return x
+        reversed_x = -1 * int(str(abs(x))[::-1])
+
+    if -1 * (2**31) <= reversed_x <= (2**31) - 1:
+        return reversed_x
     else:
         return 0
 
-
+    
 if __name__ == "__main__":
     reverse(29328987689)                                                                                                                                                                                                                                                                      
